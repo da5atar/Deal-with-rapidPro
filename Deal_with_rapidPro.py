9f6a3f3c01143dddfbf0dad27c1d3765a752ddb6
mbanje <mbanjejvernack@gmail.com>
2015-01-05 14:34:20 +0200

    Add some comments on the function used for updating rapidPro contacts

--- Deal_with_rapidPro.py
+++ Deal_with_rapidPro.py
@@ -3,14 +3,17 @@ import json
 
 
 def update_contacts():
+    #This function is used to update contacts on rapidPro
     contacts_url = 'https://rapidpro.io/api/v1/contacts.json'
+    
+    #Let's ask for the token to the user
     token = raw_input('Enter your token: ')
     
-
+    #Let's store the .json data in "data" valiable
     with open('/home/mbanje/Documents/Test/contacts-xls.json') as data_file:
         data = json.load(data_file)
 
-
+    #For earch row(for earch contact), we do a post request on the server witch is runing rapidPro
     for number_of_the_row, value in enumerate(data):
         print "We are now dealing with the row "+str(number_of_the_row)
         response4 = requests.post(contacts_url, headers={'Content-type': 'application/json', 'Authorization': 'Token %s' % token}, data = json.dumps(value))
