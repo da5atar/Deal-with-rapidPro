e364c1107ff382f34e3144dff2c5f762c02b7383
mbanje <mbanjejvernack@gmail.com>
2015-01-15 15:07:41 +0200

    An example of a function for getting contacts from RapidPro

--- Deal_with_rapidPro.py
+++ Deal_with_rapidPro.py
@@ -34,38 +34,23 @@ def list_contacts():
     #Let's ask for the token to the user
     token = raw_input('Enter your token: ')
     response = requests.get(contacts_url, headers={'Content-type': 'application/json', 'Authorization': 'Token %s' % token})
-    #pdb.set_trace();
-    cont = response.content
-    print type(cont)
+ 
     out_file = open("/home/mbanje/Documents/Test/contacts.json","w")
-    #chaine_sans_bars = response.content.replace('\','')
+   
     chaine_sans_bars = response.content
-    #chaine_sans_bars = chaine_sans_bars.replace("\"","'")
-    #chaine_sans_bars = chaine_sans_bars.replace('"','')
-    print("Tye de chaine_sans_bars")
-    print type(chaine_sans_bars)
-    #chaine_sans_bars = chaine_sans_bars.replace('\"',"'")
-    #chaine_sans_bars = chaine_sans_bars.replace('"',"")
+   
     jsone = json.loads(chaine_sans_bars)
-    print("Le json")
+
     #I can get url of the next page
     print(jsone["next"])
+
     json.dump(jsone,out_file, indent=4)
-    #json.dump(chaine_sans_bars,out_file, indent=4)
+    
 
     out_file.close()
 
-    #result=file("/home/mbanje/Documents/Test/contacts.json","r").read().replace('\"', '\'')
-    #file("/home/mbanje/Documents/Test/contacts.json","w").write(result)
 
-
-    #json_data = file("/home/mbanje/Documents/Test/contacts.json","r").read()
-    #Json2Xls('/home/mbanje/Documents/Test/contacts.xls', json_data).make()
-
-    #for n, v in enumerate(response):
-    #    print v
-
-#list_contacts = list_contacts()
+list_contacts = list_contacts()
     
 
 def list_messages():
@@ -85,5 +70,5 @@ def list_messages():
     out_file.close()
 
 
-list_messages = list_messages()
+#list_messages = list_messages()
 
